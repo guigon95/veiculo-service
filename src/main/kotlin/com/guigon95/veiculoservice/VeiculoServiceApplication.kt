8585a3bb6061f2b5f05c5e3afc1ca1ef95913029
Guilherme Gonçalves <guilherme_gon@live.com>
2024-12-10 22:27:41 -0300

    Add files via upload

---
+++ src/main/kotlin/com/guigon95/veiculoservice/VeiculoServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.guigon95.veiculoservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeiculoServiceApplication
+
+fun main(args: Array<String>) {
+	 runApplication<VeiculoServiceApplication>(*args)
+}
